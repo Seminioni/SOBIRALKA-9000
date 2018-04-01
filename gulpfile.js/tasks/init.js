6eafc7a6c8a74f12a682e05400f8d0c5ec146189
seminioni <lebatmann@gmail.com>
2018-04-02 02:36:08 +0300

    upd: add some files for package publishing;

---
+++ gulpfile.js/tasks/init.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp')
+const log = require('fancy-log')
+const colors = require('ansi-colors')
+const projectPath = require('../lib/projectPath')
+const merge = require('merge-stream')
+
+gulp.task('init', function() {
+  const srcStream = gulp.src(['src/**/*', 'src/**/.gitkeep'])
+    .pipe(gulp.dest(projectPath(PATH_CONFIG.src)))
+
+  log(colors.green('Generating default Blendid project files'))
+  log(colors.yellow(`
+To start the dev server:
+`), colors.magenta(`
+yarn run blendid
+`))
+
+  return merge(defaultStream, configStream, srcStream)
+})
